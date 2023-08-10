f9c1c74f31bd1ee66004f7de7536f50d52356293
radzhiv <radzhivkrishna@gmail.com>
2023-08-10 15:48:09 +0530

    Added minor style and js

---
+++ Numia2.0/script.js
@@ -0,0 +1,8 @@
+alert("Welcome to Pixus")
+const hello = prompt("Enter your name: ");
+
+const hey = document.getElementById("hey")
+function greet(hello){
+    hey.innerHTML = "Welcome" + " " + hello + " " + "🚀" 
+}
+greet(hello)
