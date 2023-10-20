aa5a27e7875fd992ce1bc75c95ecb026e96df499
radzhiv <radzhivkrishna@gmail.com>
2023-10-20 23:27:44 +0530

    fix: string issue fixed

--- Javascript/Projects/to-do/script.js
+++ Javascript/Projects/to-do/script.js
@@ -2,27 +2,30 @@ const todoItems = document.getElementById('todo-items')
 
 let todos = []
 
-let titleVal = document.getElementById('title')
-let descVal = document.getElementById('desc')
+let titleEl = document.getElementById('title')
+let descEl = document.getElementById('desc')
 
 let addBtn = document.getElementById('add')
 let deteteBtn = document.getElementById('delete')
 
 addBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    console.log(e)
+    // console.log(e)
     let todo = {
-        title: titleVal.value,
-        desc: descVal.value
+        title: titleEl.value,
+        desc: descEl.value
     }
     todos.push(todo)
-    let titleStr = JSON.stringify(titleVal.value)
-    let descStr = JSON.stringify(descVal.value)
+    // let titleStr = JSON.stringify(titleVal.value)
+    // console.log(titleVal.value) 
+    //  let descStr = JSON.stringify(descVal.value)
+    //  console.log(titleStr) 
+    //  console.log(descVal.value)
     let li = document.createElement('li')
     li.innerHTML = `
-    <div class="my-3 border p-2 md:w-2/3 mx-auto rounded">
-    <h2 class="font-bold border-b">${titleStr}</h2>
-    <p>${descStr}</p>
+    <div class="my-3 border p-2 md:w-2/3 mx-auto rounded" id="">
+    <h2 class="font-bold border-b">${todo.title}</h2>
+    <p>${todo.desc}</p>
     <button id='del' class="p-1 bg-red-500 rounded text-white">delete</button>
     </div>`
     todoItems.appendChild(li)
@@ -31,10 +34,8 @@ addBtn.addEventListener('click', (e) => {
 
 deteteBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    console.log(e)
+    // console.log(e)
     // todos.pop()
     todoItems.removeChild(todoItems.lastChild)
-
+    todoItems.pop()
 })
-
-
